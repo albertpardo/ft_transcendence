33d8f5375e8042e673b947568c5481128823299b
Artem Kozin <akozin@car10s1.42barcelona.com>
2025-04-21 16:22:49 +0200

    oop webserv-esque re-modelization of pong

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,61 +2,85 @@ import Fastify from 'fastify';
 import { initDB } from './db';
 import { pongMain, getPongStarted, getPongDone, getPongState } from './pong';
 
+function makeid(length : number) : string {
+	 let result : string= '';
+	 const characters : string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	 const charactersLength : number = characters.length;
+	 let counter : number = 0;
+	 while (counter < length) {
+		 result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		 counter += 1;
+	 }
+	 return result;
+}
 
 const startServer = async () => {
-  const fastify = Fastify({ logger: true });
-  const db = await initDB();
+	const fastify = Fastify({ logger: true });
+	const db = await initDB();
 
-  // inyectar la instancia de db para usarla en rutas
-  fastify.decorate('db', db);
+	// inyectar la instancia de db para usarla en rutas
+	fastify.decorate('db', db);
 
-  //GET HOME
-  fastify.get('/', async (request, reply) => {
-	  if (getPongDone() === true) {
-      	return {message : "pong's loser is: " + getPongState().stateWhoL};
-	  }
-	  if (getPongStarted() === false) {
-		  pongMain();
-		  return {message : "pong wasn't started... well, now it is!"};
-	  }
-	  return {message : "pong ongoing;", state : getPongState()};
-  });
+	//GET HOME
+	// TODO get shall have absolutely 0 body
+	// TODO gameid is ok, but userid should be the principal way of identifying users trying to do anything
+	fastify.get('/', async (request, reply) => {
+		return {message : "the request is a following structure:\n" + request + "\n"};
+		/*
+		if (!(request.hasOwnProperty("gameId"))) {
+			if ((request.hasOwnProperty("newGame"))) {
+				let localGameId : string = makeid(512);
+				return {message : "new game created.", gameId : localGameId};
+			}
+			reply.code(400);
+			return {error: "GET / must have a gameId field to identify the game the user is referrng toi; alternatively, to start a new game, pass the newGame: <anything> field"};
+		}
+		if (getPongDone(request.gameId) === true) {
+			return {message : "pong's loser is: " + getPongState(request.gameId).stateWhoL};
+		}
+		if (getPongStarted(request.gameId) === false) {
+			pongMain(request.gameId);
+			return {message : "pong wasn't started... well, now it is!"};
+		}
+		return {message : "pong ongoing;", state : getPongState(request.gameId)};
+	   */
+	});
 
-  // GET USERS
-  fastify.get('/users', async (request, reply) => {
-    const users = await db.all('SELECT * FROM users');
-    return users;
-  });
+	// GET USERS
+	fastify.get('/users', async (request, reply) => {
+		const users = await db.all('SELECT * FROM users');
+		return users;
+	});
 
-  // POST USERS
-  fastify.post('/users', async (request, reply) => {
-    const { name, email } = request.body as any;
-    try {
-      await db.run('INSERT INTO users (name, email) VALUES (?, ?)', [name, email]);
-      return { success: true };
-    } catch (err) {
-      reply.code(400);
-      return { error: 'Error inserting user', details: err };
-    }
-  });
+	// POST USERS
+	fastify.post('/users', async (request, reply) => {
+		const { name, email } = request.body as any;
+		try {
+			await db.run('INSERT INTO users (name, email) VALUES (?, ?)', [name, email]);
+			return { success: true };
+		} catch (err) {
+			reply.code(400);
+			return { error: 'Error inserting user', details: err };
+		}
+	});
 
-  // PUT USERS
-  fastify.put('/users/:id', async (request, reply) => {
-    const { id } = request.params as any;
-    const { name, email } = request.body as any;
-    try {
-      await db.run('UPDATE users SET name = ?, email = ? WHERE id = ?', [name, email, id]);
-      return { success: true };
-    } catch (err) {
-      reply.code(400);
-      return { error: 'Error updating user', details: err };
-    }
-  });
+	// PUT USERS
+	fastify.put('/users/:id', async (request, reply) => {
+		const { id } = request.params as any;
+		const { name, email } = request.body as any;
+		try {
+			await db.run('UPDATE users SET name = ?, email = ? WHERE id = ?', [name, email, id]);
+			return { success: true };
+		} catch (err) {
+			reply.code(400);
+			return { error: 'Error updating user', details: err };
+		}
+	});
 
-  // DELETE USERS
+	// DELETE USERS
 
 
-  await fastify.listen({ port: 4000, host: '0.0.0.0' });
+	await fastify.listen({ port: 4000, host: '0.0.0.0' });
 };
 
 startServer().catch(console.error);
--- backend/src/pong.ts
+++ backend/src/pong.ts
@@ -1,3 +1,13 @@
+const	ALPHA_MAX : number = 5*Math.PI/11;
+const	sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+const	FRAME_TIME : number = 1/30;
+const	FRAME_TIME_MS : number = 1000 * FRAME_TIME;
+const	WINDOW_SIZE : Vector2 = {x: 1600, y: 900};
+const	PADDLE_H : number = 100;
+const	MIN_BALL_SPEED_Y : number = 30;
+
+
+
 interface	Vector2 {
 	x: number;
 	y: number;
@@ -21,78 +31,7 @@ interface	State {
 	stateWhoL: string;
 }
 
-const	ALPHA_MAX : number = 5*Math.PI/11;
-const	sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
-const	FRAME_TIME : number = 1/30;
-const	FRAME_TIME_MS : number = 1000 * FRAME_TIME;
-const	WINDOW_SIZE : Vector2 = {x: 1600, y: 900};
-const	PADDLE_H : number = 100;
-const	MIN_BALL_SPEED_Y : number = 300;
 
-function	calculateVBounce(ball: Ball, paddle: Paddle) : Vector2 {
-	let	proportion : number = (ball.coords.y - paddle.y) / (paddle.h);
-	proportion *= 2;
-	proportion -= 1;
-	if (Math.abs(ball.speed.y) < 0.001) {
-		ball.speed.y = MIN_BALL_SPEED_Y;
-	}
-	return { x: -ball.speed.x, y: Math.sin(ALPHA_MAX * proportion) * Math.abs(ball.speed.y)};
-};
-
-function	updatePositions(ball: Ball, Lpaddle: Paddle, Rpaddle: Paddle) : void {
-	// ball bounces/fly-outs collision only check
-	if (ball.speed.x < 0) {
-		if (ball.speed.x * FRAME_TIME + ball.coords.x < 0) {
-			console.log("left exit imminent! paddle: ", Lpaddle);
-			// we will be exiting the screen if so
-			if ((ball.coords.y - Lpaddle.y >= 0) && (ball.coords.y - Lpaddle.y <= Lpaddle.h)) {
-				// bounce off the left pad, to NOT exit the screen then :)
-				ball.speed = calculateVBounce(ball, Lpaddle);
-			}
-			else {
-				// missed the pad.
-				// TODO some sort of "goal signal"? or do we control that outside?
-				// if we control that outside, then just move thru like normal.
-			}
-		}
-	}
-	else if (ball.speed.x > 0) {
-		if (ball.speed.x * FRAME_TIME + ball.coords.x > WINDOW_SIZE.x) {
-			if ((ball.coords.y - Rpaddle.y >= 0) && (ball.coords.y - Rpaddle.y <= Rpaddle.h)) {
-				// bounce off the right pad
-				ball.speed = calculateVBounce(ball, Rpaddle);
-			}
-		}
-	}
-	if (ball.speed.y < 0) {
-		if (ball.speed.y * FRAME_TIME + ball.coords.y < 0) {
-			ball.speed.y *= -1;
-		}
-	}
-	else if (ball.speed.y > 0) {
-		if (ball.speed.y * FRAME_TIME + ball.coords.y > WINDOW_SIZE.y) {
-			ball.speed.y *= -1;
-		}
-	}
-	ball.coords.x += ball.speed.x * FRAME_TIME;
-	ball.coords.y += ball.speed.y * FRAME_TIME;
-
-	Lpaddle.y += Lpaddle.speed * FRAME_TIME;
-	Rpaddle.y += Rpaddle.speed * FRAME_TIME;
-	// clamp the paddles
-	if (Lpaddle.y < 0) {
-		Lpaddle.y = 0;
-	}
-	if (Rpaddle.y < 0) {
-		Rpaddle.y = 0;
-	}
-	if (Lpaddle.y + Lpaddle.h > WINDOW_SIZE.y) {
-		Lpaddle.y = WINDOW_SIZE.y - Lpaddle.h;
-	}
-	if (Rpaddle.y + Rpaddle.h > WINDOW_SIZE.y) {
-		Rpaddle.y = WINDOW_SIZE.y - Rpaddle.h;
-	}
-}
 
 function	checkLoseConditions(ball: Ball) : string {
 	if (ball.coords.x < 0) {
@@ -104,43 +43,131 @@ function	checkLoseConditions(ball: Ball) : string {
 	return "none";
 }
 
-let ourBall : Ball = { speed: {x: -1000, y: 0}, coords: {x: WINDOW_SIZE.x/2, y: WINDOW_SIZE.y/2}};
-let ourLpaddle : Paddle = { y: (WINDOW_SIZE.y - PADDLE_H)/2, h: PADDLE_H, speed: 0 };
-let ourRpaddle : Paddle = { y: 430, h: PADDLE_H, speed: 0 };
-//let ourRpaddle : Paddle = { y: (WINDOW_SIZE.y - PADDLE_H)/2, h: PADDLE_H, speed: 0 };
-let	whoLost : string = "none";
-let	state : State = { stateBall: ourBall, stateLP: ourLpaddle, stateRP: ourRpaddle, stateWhoL: whoLost };
-let pongStarted : boolean = false;
-let pongDone : boolean = false;
+function	calculateVBounce(ball: Ball, paddle: Paddle) : Vector2 {
+	let	proportion : number = (ball.coords.y - paddle.y) / (paddle.h);
+	proportion *= 2;
+	proportion -= 1;
+	if (Math.abs(ball.speed.y) < 0.001) {
+		ball.speed.y = MIN_BALL_SPEED_Y;
+	}
+	return { x: -ball.speed.x, y: Math.sin(ALPHA_MAX * proportion) * Math.abs(ball.speed.y)};
+};
 
-export const	pongMain = async () => {
-	pongStarted = true;
-	while (true) {
-		whoLost = checkLoseConditions(ourBall);
-		if (whoLost != "none") {
-			pongDone = true;
-			state = { stateBall: ourBall, stateLP: ourLpaddle, stateRP: ourRpaddle, stateWhoL: whoLost };
-			return ("ball lost by... " + whoLost);
-			// in real world, this here would restart the game instead of just breaking.
+
+
+class	PongRuntime {
+	private ball : Ball = { speed: {x: -100, y: 0}, coords: {x: WINDOW_SIZE.x/2, y: WINDOW_SIZE.y/2}};
+	private Lpaddle : Paddle = { y: (WINDOW_SIZE.y - PADDLE_H)/2, h: PADDLE_H, speed: 0 };
+	private Rpaddle : Paddle = { y: 430, h: PADDLE_H, speed: 0 };
+	private	whoLost : string = "none";
+
+	public gstate : State = { stateBall: this.ball, stateLP: this.Lpaddle, stateRP: this.Rpaddle, stateWhoL: this.whoLost };
+	public pongStarted : boolean = false;
+	public pongDone : boolean = false;
+
+	private updatePositions() : void {
+		// ball bounces/fly-outs collision only check
+		if (this.ball.speed.x < 0) {
+			if (this.ball.speed.x * FRAME_TIME + this.ball.coords.x < 0) {
+				console.log("left exit imminent! paddle: ", this.Lpaddle);
+				// we will be exiting the screen if so
+				if ((this.ball.coords.y - this.Lpaddle.y >= 0) && (this.ball.coords.y - this.Lpaddle.y <= this.Lpaddle.h)) {
+					// bounce off the left pad, to NOT exit the screen then :)
+					this.ball.speed = calculateVBounce(this.ball, this.Lpaddle);
+				}
+				else {
+					// missed the pad.
+					// TODO some sort of "goal signal"? or do we control that outside?
+					// if we control that outside, then just move thru like normal.
+				}
+			}
 		}
-		updatePositions(ourBall, ourLpaddle, ourRpaddle);
-		console.log("ball speed: ", ourBall.speed);
-		console.log("ball coords:", ourBall.coords);
-		state = { stateBall: ourBall, stateLP: ourLpaddle, stateRP: ourRpaddle, stateWhoL: whoLost };
-		await sleep(FRAME_TIME_MS);
+		else if (this.ball.speed.x > 0) {
+			if (this.ball.speed.x * FRAME_TIME + this.ball.coords.x > WINDOW_SIZE.x) {
+				if ((this.ball.coords.y - this.Rpaddle.y >= 0) && (this.ball.coords.y - this.Rpaddle.y <= this.Rpaddle.h)) {
+					// bounce off the right pad
+					this.ball.speed = calculateVBounce(this.ball, this.Rpaddle);
+				}
+			}
+		}
+		if (this.ball.speed.y < 0) {
+			if (this.ball.speed.y * FRAME_TIME + this.ball.coords.y < 0) {
+				this.ball.speed.y *= -1;
+			}
+		}
+		else if (this.ball.speed.y > 0) {
+			if (this.ball.speed.y * FRAME_TIME + this.ball.coords.y > WINDOW_SIZE.y) {
+				this.ball.speed.y *= -1;
+			}
+		}
+		this.ball.coords.x += this.ball.speed.x * FRAME_TIME;
+		this.ball.coords.y += this.ball.speed.y * FRAME_TIME;
+
+		this.Lpaddle.y += this.Lpaddle.speed * FRAME_TIME;
+		this.Rpaddle.y += this.Rpaddle.speed * FRAME_TIME;
+		// clamp the paddles
+		if (this.Lpaddle.y < 0) {
+			this.Lpaddle.y = 0;
+		}
+		if (this.Rpaddle.y < 0) {
+			this.Rpaddle.y = 0;
+		}
+		if (this.Lpaddle.y + this.Lpaddle.h > WINDOW_SIZE.y) {
+			this.Lpaddle.y = WINDOW_SIZE.y - this.Lpaddle.h;
+		}
+		if (this.Rpaddle.y + this.Rpaddle.h > WINDOW_SIZE.y) {
+			this.Rpaddle.y = WINDOW_SIZE.y - this.Rpaddle.h;
+		}
 	};
+
+	public mainLoop = async () => {
+		this.pongStarted = true;
+		while (true) {
+			this.whoLost = checkLoseConditions(this.ball);
+			if (this.whoLost != "none") {
+				this.pongDone = true;
+				this.gstate = { stateBall: this.ball, stateLP: this.Lpaddle, stateRP: this.Rpaddle, stateWhoL: this.whoLost };
+				return ("ball lost by... " + this.whoLost);
+				// in real world, this here would restart the game instead of just breaking.
+			}
+			this.updatePositions();
+			console.log("ball speed: ", this.ball.speed);
+			console.log("ball coords:", this.ball.coords);
+			this.gstate = { stateBall: this.ball, stateLP: this.Lpaddle, stateRP: this.Rpaddle, stateWhoL: this.whoLost };
+			await sleep(FRAME_TIME_MS);
+		};
+	};
+};
+
+const gamesMap = new Map();
+
+export const	pongMain = async (gameId: string) => {
+	if (!(gamesMap.has(gameId))) {
+		gamesMap.set(gameId, new PongRuntime);
+		gamesMap.get(gameId).mainLoop();
+	}
+	else {
+		throw ("you're already registered at " + gameId);
+	}
 }
 
-export function	getPongStarted() : boolean {
-	return (pongStarted);
+export function	getPongStarted(gameId: string) : boolean {
+	if (gamesMap.has(gameId)) {
+		return (gamesMap.get(gameId).pongStarted);
+	}
+	throw ("no game found registered at " + gameId);
 }
 
-export function	getPongDone() : boolean {
-	return (pongDone);
+export function	getPongDone(gameId: string) : boolean {
+	if (gamesMap.has(gameId)) {
+		return (gamesMap.get(gameId).pongDone);
+	}
+	throw ("no game found registered at " + gameId);
 }
 
-export function	getPongState() : State {
-	return (state);
+export function	getPongState(gameId: string) : State {
+	if (gamesMap.has(gameId)) {
+		return (gamesMap.get(gameId).pongState);
+	}
+	throw ("no game found registered at " + gameId);
 }
-
-//pongMain().catch(console.error);
