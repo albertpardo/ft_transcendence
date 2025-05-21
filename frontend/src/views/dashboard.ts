19c7797af3e3a0443f354f391cb125c72559732a
Artem Kozin <akozin@car7s3.42barcelona.com>
2025-05-21 13:31:58 +0200

    I broke the alignment, trying to fix

--- frontend/src/views/dashboard.ts
+++ frontend/src/views/dashboard.ts
@@ -121,19 +121,23 @@ export function initDashboard() {
       p-4 md:p-6 lg:p-8 xl:p-12 
       min-h-screen overflow-auto
       bg-gray-900
-    "></main>
-    <!-- Hidden Game Area -->
-    <button id="secret-button" class="mt-auto w-full p-3 bg-red-600 rounded-lg hover:bg-red-700 transition" hidden>click me bro</button>    
-    <div id="game-window" hidden>
-    <svg width="1280px" height="720px">
+    ">
+
+      <!-- Hidden Game Area -->
+      <button id="secret-button" class="mt-auto w-full p-3 bg-red-600 rounded-lg hover:bg-red-700 transition" hidden>click me bro</button>    
+
+      <div id="game-window" hidden>
+      <svg width="1280px" height="720px">
         <rect width="100%" height="100%" fill="red" />
         <circle cx="100%" cy="100%" r="150" fill="blue" stroke="black" />
         <polygon points="120,0 240,225 0,625" fill="green"/>
         <text id="game-text" x="50" y="100" font-family="Verdana" font-size="55" fill="white" stroke="black" stroke-width="2">
         Hello!
         </text>
-    </svg>
-    </div>
+      </svg>
+      </div>
+    
+    </main>
   `;
 
   // Mobile menu functionality
