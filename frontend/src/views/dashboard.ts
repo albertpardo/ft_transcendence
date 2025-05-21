4b2d8e3ab8f6a722ac309953c55613073fc4686b
Artem Kozin <akozin@car7s3.42barcelona.com>
2025-05-21 14:31:11 +0200

    still bad

--- frontend/src/views/dashboard.ts
+++ frontend/src/views/dashboard.ts
@@ -122,22 +122,20 @@ export function initDashboard() {
       min-h-screen overflow-auto
       bg-gray-900
     ">
-
-      <!-- Hidden Game Area -->
-      <button id="secret-button" class="mt-auto w-full p-3 bg-red-600 rounded-lg hover:bg-red-700 transition" hidden>click me bro</button>    
-
-      <div id="game-window" hidden>
-      <svg width="1280px" height="720px">
-        <rect width="100%" height="100%" fill="red" />
-        <circle cx="100%" cy="100%" r="150" fill="blue" stroke="black" />
-        <polygon points="120,0 240,225 0,625" fill="green"/>
-        <text id="game-text" x="50" y="100" font-family="Verdana" font-size="55" fill="white" stroke="black" stroke-width="2">
-        Hello!
-        </text>
-      </svg>
-      </div>
-    
     </main>
+
+    <!-- Hidden Game Area -->
+    <button id="secret-button" class="mt-auto w-full p-3 bg-red-600 rounded-lg hover:bg-red-700 transition" hidden>click me bro</button>    
+    <div id="game-window" hidden>
+    <svg width="1280px" height="720px">
+      <rect width="100%" height="100%" fill="red" />
+      <circle cx="100%" cy="100%" r="150" fill="blue" stroke="black" />
+      <polygon points="120,0 240,225 0,625" fill="green"/>
+      <text id="game-text" x="50" y="100" font-family="Verdana" font-size="55" fill="white" stroke="black" stroke-width="2">
+      Hello!
+      </text>
+    </svg>
+    </div>
   `;
 
   // Mobile menu functionality
