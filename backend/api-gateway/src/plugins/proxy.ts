b176314a4d4a5fc5b23837ef8a211913fd0c9874
Boris Randebrock <brandebr@student.42barcelona.com>
2025-06-05 18:38:29 +0200

    dotenv proxy USER_MANAGEMENT_URL

--- backend/api-gateway/src/plugins/proxy.ts
+++ backend/api-gateway/src/plugins/proxy.ts
@@ -7,21 +7,23 @@ import { Readable } from 'stream';
 export default fp(async function (fastify: FastifyInstance) {
     // register proxy: without onResponse
     fastify.register(fastifyHttpProxy, {
-        upstream: 'http://user_management:9001',
+        
+        upstream: process.env.USER_MANAGEMENT_URL,
+        //upstream: 'http://user_management:9001',
         prefix: '/api/login',
         rewritePrefix: '/api/user/login',
         http2: false,
     });
 
     fastify.register(fastifyHttpProxy, {
-        upstream: 'http://user_management:9001',
+        upstream: process.env.USER_MANAGEMENT_URL,
         prefix: '/api/signup',
         rewritePrefix: '/api/user/signup',
         http2: false
     });
 
     fastify.register(fastifyHttpProxy, {
-        upstream: 'http://user_management:9001',
+        upstream: process.env.USER_MANAGEMENT_URL,
         prefix: '/api/profile',
         rewritePrefix: '/api/user/profile',
         http2: false
