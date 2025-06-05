8feef54408ffe0a277398bb73a9c3ac50d2fd20b
Lin Yao <linyao@cbr12s4.42barcelona.com>
2025-06-05 16:42:02 +0200

    logic of update&delete in user module added

--- backend/microservices/user_management/services/userService.js
+++ backend/microservices/user_management/services/userService.js
@@ -59,3 +59,50 @@ exports.getProfile = async (userId) => {
         password: user.password
     };
 }
+
+exports.updateProfile = async (userId, { nickname, email, password, avatar }) => {
+    const user = db.getUserById(userId);
+    if (!user) return { error: "User not found" };
+
+    const updateFields = {
+        nickname: nickname ?? user.nickname,
+        email: email ?? user.email,
+        password: user.password,
+        avatar: avatar ?? user.avatar
+    }
+
+    /*
+    const existing = db.getUserByUsernameOrEmail(username, email);
+    if (existing && existing.id !== userId) {
+        return { error: "Username or email already taken" };
+    }
+    */
+    if (nickname && nickname !== user.nickname) {
+        const nickexist = db.getNickname(nickname);
+        if (nickexist && nickexist.id !== userId) {
+            return { error: "Nickname already in use" };
+        }
+    }
+
+    if (password && password !== "") {
+        updateFields.password = await bcrypt.hash(password, 10);
+    }
+
+    if (email && email !== user.email) {
+        const existing = db.getUserByUsernameOrEmail(user.username, email);
+        if (existing && existing.id !== userId) {
+            return { error: "Email alrealdy in use" };
+        }
+    }
+
+    db.updateUser(userId, updateFields);
+    return { success: true };
+}
+
+exports.deleteProfile = async (userId) => {
+    const user = db.getUserById(userId);
+    if (!user) return { error: "User not found" };
+
+    db.deleteUser(userId);
+    return { success: true };
+}
