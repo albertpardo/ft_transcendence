e15a9441c813ba845d208fe6165f34b9a0c17201
Artem Kozin <akozin@car6s1.42barcelona.com>
2025-06-11 13:50:28 +0200

    function to add matches to the table

--- backend/microservices/game_service/src/history.ts
+++ backend/microservices/game_service/src/history.ts
@@ -1,11 +1,12 @@
 // src/history.ts
 import Database from 'better-sqlite3';
+const db = Database('/app/dbs/history.db');
 
 export const historyMain = async () => {
-  const db = Database('/app/dbs/history.db');
   const init = db.prepare(`
   CREATE TABLE IF NOT EXISTS matches (
-    id TEXT PRIMARY KEY UNIQUE,
+    id NUMBER PRIMARY KEY AUTOINCREMENT,
+    gameId TEXT,
     date NUMBER,
     leftId TEXT,
     rightId TEXT,
@@ -15,4 +16,9 @@ export const historyMain = async () => {
   )`);
   init.run();
   console.log(db);
+}
+
+export const addMatch = async (gameId : string, leftId : string, rightId : string, scoreL : number, scoreR : number) => {
+  const init = db.prepare("INSERT INTO matches (gameId, date, leftId, rightId, scoreL, scoreR, state) VALUES (?, ?, ?, ?, ?, ?, ?)");
+  const info = init.run(gameId, Date.now(), leftId, rightId, scoreL, scoreR, (scoreL > scoreR ? "left" : "right"));
 }
