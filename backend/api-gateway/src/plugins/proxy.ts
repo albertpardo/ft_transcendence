4277b19ceb01e3569eaaaaca9003bcf2fae5127e
Boris <boris@randebrock.com>
2025-06-27 13:59:25 +0200

    Update proxy.ts

--- backend/api-gateway/src/plugins/proxy.ts
+++ backend/api-gateway/src/plugins/proxy.ts
@@ -127,6 +127,7 @@ export default fp(async function (fastify: FastifyInstance): Promise<void> {
                 let body;
                 if (payload && typeof (payload as Readable).read === 'function') {
                     console.log("if number 1");
+                    console.log("*************", payload);
                     const raw: Buffer = await getRawBody(payload as Readable);
                     console.log("getrawbody success");
                     console.log(raw);
