ea8bd3806056211a5a4bd770c40d47b6a91e55a4
apm <apm@mail.com>
2025-07-19 17:14:42 +0200

    TODO:test 'log_format.js' in 'user_management'

--- backend/microservices/user_management/pino_utils/log_format.js
+++ backend/microservices/user_management/pino_utils/log_format.js
@@ -1,8 +1,38 @@
-const logFormat = (source, msg) => {
-    return {
-        source,
-        message: msg
-    };
-};
+const stringify = require('safe-stable-stringify');
+
+function formatString(msg) {
+  if (typeof msg === 'string') {
+    return msg;
+  }
+
+  if (msg instanceof Error) {
+    // Extracts its own properties and those of the prototype
+    const { name, message, stack } = msg;
+
+    const extraEntries = Object.entries(msg).filter(
+      ([key]) => !['name', 'message', 'stack'].includes(key)
+    );
+
+    const extraProps = extraEntries.length
+      ? `\nExtra: ${stringify(Object.fromEntries(extraEntries))}`
+      : '';
+
+    return `${name}: ${message}${stack ? '\n' + stack : ''}${extraProps}`;
+  }
+
+  try {
+    return stringify(msg) ?? String(msg);
+  } catch {
+    return String(msg);
+  }
+}
+
+function logFormat(source, msg) {
+  return {
+    source,
+    message: formatString(msg)
+  };
+}
 
 module.exports = logFormat;
+
