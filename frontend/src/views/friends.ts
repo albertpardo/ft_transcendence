66a4b4f12f11424eb2e9be571a0cf4336556a2d3
apardo-m <apardo-m@student.42barcelona.com>
2025-08-28 17:02:15 +0200

    Refresh txt translated

--- frontend/src/views/friends.ts
+++ frontend/src/views/friends.ts
@@ -42,8 +42,8 @@ export async function renderFriendsContent(hideableElements) {
 
   let friendsData;
   el.innerHTML = `
-    <div class="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
-      <h1 class="text-3xl font-bold mb-6">${t("friends")}</h1>
+    <div class="w-full flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
+      <h1 class="w-full text-3xl font-bold mb-6 whitespace-nowrap">${t("friends")}</h1>
         <button id="refresh"
           class="w-full p-2 bg-blue-500 rounded-lg hover:bg-blue-400 transition text-white font-medium disabled:border-gray-200 disabled:bg-gray-700 disabled:text-gray-500 disabled:shadow-none">
           ${t("friendsTxt.refresh")}
